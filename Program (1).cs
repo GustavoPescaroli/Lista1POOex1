113ab3cb7bc7a8996c884879b0031c59a78dc913
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-08-25 12:32:10 -0300

    Add files via upload

---
+++ Program (1).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOOex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r;
+            r = new Retangulo();
+
+           
+            Console.WriteLine("Digite o valor da base");
+            r.setb(double.Parse(Console.ReadLine()));
+            Console.WriteLine("Digite o valor da altura");
+            r.seth(double.Parse(Console.ReadLine()));
+            r.calcular();
+            Console.WriteLine("O valor da base {0} da altura {1} área é de {2}",r.getb(),r.geth(), r.geta());
+
+
+
+
+        }
+    }
+}
